7bbc3ff26fb49eebdbd09354eb90e611dd604d06
Sharriff Dyer II <30534530+Dachief332@users.noreply.github.com>
2020-04-28 22:57:11 -0400

    Add files via upload

---
+++ Animal.cpp
@@ -0,0 +1,18 @@
+#include "Animal.h"
+
+string Animal::getAnimalType() 
+{
+	return name;
+};
+string Animal::getName()
+{
+	return name;
+};
+void Animal::setName(string name)
+{
+	this->name = name;
+};
+void Animal::makeNoise()
+{
+
+};
